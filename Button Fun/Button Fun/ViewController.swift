9ec8830377d11dd03e6790345ae2fddeb9f59258
great-tiger <892873387@qq.com>
2016-02-18 22:24:51 +0800

    temp

--- Button Fun/Button Fun/ViewController.swift
+++ Button Fun/Button Fun/ViewController.swift
@@ -9,15 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    /*
+        在使用auto layout的时候label一直有橙色线，不知为何。
+        问题还真不好找，是因为label中的默认值，被我删掉了。加上就可以了。
+    */
+    
+    @IBAction func btnPressed(sender: UIButton) {
+      let title=sender.titleForState(.Normal)!;
+      let plainText="\(title) button pressed";
+       statusLabel.text=plainText;
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
 }
 
